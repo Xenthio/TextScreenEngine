412e152887fc1ba4fe38cfd4772a5fc67ed40682
Xenthio <thegooster88@gmail.com>
2020-06-04 12:47:09 +1000

    Code restructure

---
+++ engine.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#define WIDTH 600
+#define HEIGHT 300
+#define IMG_PATH "font.png"
+#define NEWLINE_PATH "newLine.png"
+
+extern SDL_Window *win;
+extern SDL_Texture *img;
+extern SDL_Texture *images[64];
+extern int count;
+extern int charX;
+extern int charY;
+extern SDL_Renderer *renderer;
+extern SDL_Texture *font;
+extern SDL_Texture *newLineImage;
+extern SDL_Rect newLine;
+extern char codePage[128];
+
+extern bool operator==(const SDL_Rect& a1, const SDL_Rect& a2);
+
+extern SDL_Texture* GetAreaTextrue(SDL_Rect rect, SDL_Renderer* renderer, SDL_Texture* source);
+
+extern SDL_Rect codePageDecode(char letter);
+
+extern void drawChar(char letter);
+
+extern void drawChars(char* str);
+
+extern int initWindow();
+extern int mainLoop();
